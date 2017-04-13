9bac1525df8d0e6c7a9e37b7056faeda442ae86a
yaphone <zhouyaphone@163.com>
2017-04-13 23:54:00 +0800

    日常开发

--- src/test/java/cn/zhouyafeng/itchat4j/AnotherSimpleTest.java
+++ src/test/java/cn/zhouyafeng/itchat4j/AnotherSimpleTest.java
@@ -4,14 +4,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.Reader;
-import java.util.Iterator;
-import java.util.Map.Entry;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.util.IOUtils;
-
-import cn.zhouyafeng.itchat4j.utils.Tools;
 
 public class AnotherSimpleTest {
 	public static void main(String[] args) {
@@ -22,11 +18,13 @@ public class AnotherSimpleTest {
 			r = new FileReader(file);
 			String text = IOUtils.readAll(r);
 			JSONObject obj = JSON.parseObject(text);
-			Long InviteStartCount = obj.getLong("InviteStartCount");
+			System.out.println(obj.get("SyncKey"));
+			// Long InviteStartCount = obj.getLong("InviteStartCount");
 			// System.out.println(InviteStartCount);
 			// System.out.println(obj.get("User"));
-			Iterator<Entry<String, Object>> it = ((JSONObject) obj.get("User")).entrySet().iterator();
-			Tools.structFriendInfo(obj);
+			// Iterator<Entry<String, Object>> it = ((JSONObject)
+			// obj.get("User")).entrySet().iterator();
+			// Tools.structFriendInfo(obj);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
