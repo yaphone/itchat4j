67af9ef0b359ec22d2c3129a79989a2a314c814a
mythos <kuangchengping@outlook.com>
2017-06-14 18:52:17 +0800

    路径问题

--- read.sh
+++ read.sh
@@ -1,6 +1,7 @@
+basepath=$(cd `dirname $0`; pwd)
 rm temp
 touch temp
-java -cp JavaToolKit-1.0-SNAPSHOT.jar com.myth.qrcode.ReadQRCode /home/kcp/Code/wechat/itchat4j/QR.jpg>>temp
+java -cp JavaToolKit-1.0-SNAPSHOT.jar com.myth.qrcode.ReadQRCode $basepath/QR.jpg>>temp
 
 infile=temp
 while read xcoord; do
