b68bff976a4c1ee7f45e28cc885db46996645535
yaphone <zhouyafeng2013@gmail.com>
2024-04-04 19:45:19 +0800

    添加UOS Header，解决“为了保障你的账号安全，暂不支持使用网页版微信”问题
    添加UOS Header，解决“为了保障你的账号安全，暂不支持使用网页版微信”问题

--- src/main/java/cn/zhouyafeng/itchat4j/service/impl/LoginServiceImpl.java
+++ src/main/java/cn/zhouyafeng/itchat4j/service/impl/LoginServiceImpl.java
@@ -486,7 +486,11 @@ public class LoginServiceImpl implements ILoginService {
 			String text = "";
 
 			try {
-				HttpEntity entity = myHttpClient.doGet(originalUrl, null, false, null);
+				Map<String, String> header = new HashMap<>();
+				header.put("client-version","2.0.0");
+				header.put("extspam","Go8FCIkFEokFCggwMDAwMDAwMRAGGvAESySibk50w5Wb3uTl2c2h64jVVrV7gNs06GFlWplHQbY/5FfiO++1yH4ykCyNPWKXmco+wfQzK5R98D3so7rJ5LmGFvBLjGceleySrc3SOf2Pc1gVehzJgODeS0lDL3/I/0S2SSE98YgKleq6Uqx6ndTy9yaL9qFxJL7eiA/R3SEfTaW1SBoSITIu+EEkXff+Pv8NHOk7N57rcGk1w0ZzRrQDkXTOXFN2iHYIzAAZPIOY45Lsh+A4slpgnDiaOvRtlQYCt97nmPLuTipOJ8Qc5pM7ZsOsAPPrCQL7nK0I7aPrFDF0q4ziUUKettzW8MrAaiVfmbD1/VkmLNVqqZVvBCtRblXb5FHmtS8FxnqCzYP4WFvz3T0TcrOqwLX1M/DQvcHaGGw0B0y4bZMs7lVScGBFxMj3vbFi2SRKbKhaitxHfYHAOAa0X7/MSS0RNAjdwoyGHeOepXOKY+h3iHeqCvgOH6LOifdHf/1aaZNwSkGotYnYScW8Yx63LnSwba7+hESrtPa/huRmB9KWvMCKbDThL/nne14hnL277EDCSocPu3rOSYjuB9gKSOdVmWsj9Dxb/iZIe+S6AiG29Esm+/eUacSba0k8wn5HhHg9d4tIcixrxveflc8vi2/wNQGVFNsGO6tB5WF0xf/plngOvQ1/ivGV/C1Qpdhzznh0ExAVJ6dwzNg7qIEBaw+BzTJTUuRcPk92Sn6QDn2Pu3mpONaEumacjW4w6ipPnPw+g2TfywJjeEcpSZaP4Q3YV5HG8D6UjWA4GSkBKculWpdCMadx0usMomsSS/74QgpYqcPkmamB4nVv1JxczYITIqItIKjD35IGKAUwAA==");
+				originalUrl = originalUrl +"&fun=new&version=v2&mod=desktop&lang=zh_CN";
+				HttpEntity entity = myHttpClient.doGet(originalUrl, null, false, header);
 				text = EntityUtils.toString(entity);
 			} catch (Exception e) {
 				LOG.info(e.getMessage());
--- src/test/java/cn/zhouyafeng/itchat4j/demo/demo1/MyTest.java
+++ src/test/java/cn/zhouyafeng/itchat4j/demo/demo1/MyTest.java
@@ -3,6 +3,8 @@ package cn.zhouyafeng.itchat4j.demo.demo1;
 import cn.zhouyafeng.itchat4j.Wechat;
 import cn.zhouyafeng.itchat4j.face.IMsgHandlerFace;
 
+import java.io.File;
+
 /**
  *
  * @author https://github.com/yaphone
@@ -13,6 +15,17 @@ import cn.zhouyafeng.itchat4j.face.IMsgHandlerFace;
 public class MyTest {
 	public static void main(String[] args) {
 		String qrPath = "D://itchat4j//login"; // 保存登陆二维码图片的路径，这里需要在本地新建目录
+		File folder = new File(qrPath);
+		if (!folder.exists()) {
+			boolean success = folder.mkdirs();
+			if (success) {
+				System.out.println("文件夹创建成功");
+			} else {
+				System.out.println("文件夹创建失败");
+			}
+		} else {
+			System.out.println("文件夹已存在");
+		}
 		IMsgHandlerFace msgHandler = new SimpleDemo(); // 实现IMsgHandlerFace接口的类
 		Wechat wechat = new Wechat(msgHandler, qrPath); // 【注入】
 		wechat.start(); // 启动服务，会在qrPath下生成一张二维码图片，扫描即可登陆，注意，二维码图片如果超过一定时间未扫描会过期，过期时会自动更新，所以你可能需要重新打开图片
